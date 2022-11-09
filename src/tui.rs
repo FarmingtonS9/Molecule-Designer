b7253a45a569fa1718076c9404aa21e509c13a0f
FarmingtonS9 <sframpton40@gmail.com>
2022-11-09 22:24:11 +1100

    Update tui.rs

--- src/tui.rs
+++ src/tui.rs
@@ -1,14 +1,19 @@
 use std::{io, thread, time::Duration};
 
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use tui::{
-    backend::CrosstermBackend,
-    widgets::{Block, Borders},
-    Terminal,
+    backend::{Backend, CrosstermBackend},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::Color,
+    widgets::{
+        canvas::{Canvas, Map, MapResolution},
+        Block, Borders,
+    },
+    Frame, Terminal,
 };
 
 pub fn tui_setup() -> Result<(), io::Error> {
@@ -19,13 +24,7 @@ pub fn tui_setup() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
-
-    thread::sleep(Duration::from_millis(5000));
+    let res = run_app(&mut terminal);
 
     // restore terminal
     disable_raw_mode()?;
@@ -37,4 +36,50 @@ pub fn tui_setup() -> Result<(), io::Error> {
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f))?;
+
+        if let Event::Key(key) = event::read()? {
+            if let KeyCode::Char('q') = key.code {
+                return Ok(());
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let canvas = Canvas::default()
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title("Molecule Designer")
+                .title_alignment(Alignment::Center),
+        )
+        .paint(|ctx| {
+            ctx.draw(&Map {
+                color: Color::White,
+                resolution: MapResolution::High,
+            })
+        });
+    f.render_widget(canvas, f.size());
+
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(
+            [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    let block = Block::default().title("Element").borders(Borders::ALL);
+    f.render_widget(block, chunks[0]);
+    let block = Block::default().title("Particle").borders(Borders::ALL);
+    f.render_widget(block, chunks[2]);
 }
